d29fbb064b2ac26a41bd036bfe388beb3afc7761
rtavares <ricardotavaresit@gmail.com>
2022-06-01 19:52:01 +0100

    create list-tasks-component component

---
+++ src/components/Tasks/ListTasksComponent.vue
@@ -0,0 +1,18 @@
+<template>
+  <div>
+    {{ title }}
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      title: "Tasks List",
+    };
+  },
+};
+</script>
+
+<style scoped>
+</style>
