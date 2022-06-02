7274b3e2e528865068fa2b90f8569d7bb1fcb3dd
rtavares <ricardotavaresit@gmail.com>
2022-06-02 20:27:27 +0100

    create task list

--- src/components/Tasks/ListTasksComponent.vue
+++ src/components/Tasks/ListTasksComponent.vue
@@ -1,6 +1,26 @@
 <template>
   <div>
     <h2>{{ title }}</h2>
+
+    <table class="table">
+      <thead>
+        <tr>
+          <th>Id</th>
+          <th>Name</th>
+          <th class="actions">Actions</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr v-bind:key="index" v-for="(task, index) in tasks">
+          <td>{{ task.id }}</td>
+          <td>{{ task.name }}</td>
+          <td>
+            <a class="btn btn-primary" href="">Edit</a>
+            <a class="btn btn-danger" href="">Delete</a>
+          </td>
+        </tr>
+      </tbody>
+    </table>
   </div>
 </template>
 
@@ -9,10 +29,20 @@ export default {
   data() {
     return {
       title: "Tasks List",
+      tasks: [
+        { id: 1, name: "Task 1" },
+        { id: 2, name: "Task 2" },
+        { id: 3, name: "Task 3" },
+        { id: 4, name: "Task 4" },
+        { id: 5, name: "Task 5" },
+      ],
     };
   },
 };
 </script>
 
 <style scoped>
+.actions {
+  width: 150px;
+}
 </style>
