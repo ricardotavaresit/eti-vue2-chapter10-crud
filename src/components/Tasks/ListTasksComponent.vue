100f2e2407da72979a72e477aa0f4b3cf1ad0eda
rtavares <ricardotavaresit@gmail.com>
2022-06-03 21:20:28 +0100

    form create

--- src/components/Tasks/ListTasksComponent.vue
+++ src/components/Tasks/ListTasksComponent.vue
@@ -2,6 +2,11 @@
   <div>
     <h2>{{ title }}</h2>
 
+    <form class="form form-inline" v-on:submit.prevent="save">
+      <input type="text" placeholder="Task name" v-model="task.name" />
+      <button type="submit" class="btn btn-primary">Send</button>
+    </form>
+
     <table class="table">
       <thead>
         <tr>
@@ -36,7 +41,23 @@ export default {
         { id: 4, name: "Task 4" },
         { id: 5, name: "Task 5" },
       ],
+      task: {
+        id: "",
+        name: "",
+      },
     };
+  },
+  methods: {
+    save() {
+      this.tasks.push({
+        id: this.tasks.length + 1,
+        name: this.task.name,
+      });
+      this.task = {
+        id: "",
+        name: "",
+      };
+    },
   },
 };
 </script>
@@ -44,5 +65,9 @@ export default {
 <style scoped>
 .actions {
   width: 150px;
+}
+
+form {
+  margin: 20px 0;
 }
 </style>
